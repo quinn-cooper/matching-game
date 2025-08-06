baaaeced499a2b5442847e15e8fc6453b488c97f
Quinn Cooper <quinncooper88@gmail.com>
2025-08-05 19:58:21 -0700

    Removed timer from html, added clickCard and statsCounter methods for allowing users to check cards, to check for matches/mismatches, and for counter increments. Also started styling the buttons.

--- scripts/game.js
+++ scripts/game.js
@@ -56,8 +56,11 @@ class Game {
         this.gameOver = document.getElementById('game-over');
         this.winGame = document.getElementById('win-game');
         this.cardDesign = document.querySelectorAll('.card-design');
-        this.cardImage = document.querySelectorAll('.card-image')
+        this.cardImage = document.querySelectorAll('.card-image');
 
+        // Counters
+        this.matchCounter = document.getElementById('match-counter');
+        this.mismatchCounter = document.getElementById('mismatch-counter');
 
         // Buttons
         this.newGameBtn = document.querySelectorAll('.new-game');
@@ -66,8 +69,12 @@ class Game {
 
         // Setup
         this.cards = cards;
-        this.flipCard = [];
+        this.flippedCard = [];
+        this.preventOpen = false;
+        this.isProcessing = false;
+        this.canClick = true;
         this.turns = 0;
+        this.cardsPerTurn = 0;
         this.matches = 0;
         this.mismatches = 0;
         this.maxMatches = 9;
@@ -85,11 +92,14 @@ class Game {
         this.gameboard.style.display = 'none';
         this.gameOver.style.display = 'none';
         this.winGame.style.display = 'none';
+        this.mainMenu.style.display = 'block';
     }
 
     initBtns() {
+        // Instructions button
         this.infoBtn.addEventListener('click', () => {
             this.mainMenu.style.display = 'none';
+            this.gameboard.style.display = 'none';
             this.gameOver.style.display = 'none';
             this.winGame.style.display = 'none';
             this.instructions.style.display = 'block';
@@ -118,7 +128,8 @@ class Game {
         this.gameboard.style.display = 'block';
 
         // Reset the following
-        this.flipCard = [];
+        this.flippedCard = [];
+        this.isProcessing = false;
         this.turns = 0;
         this.matches = 0;
         this.mismatches = 0;
@@ -142,24 +153,78 @@ class Game {
         const htmlCards = this.gameboard.querySelectorAll('.card');
         htmlCards.forEach((cardEl, i) => {
             const card = this.cards[i];
-            cardEl.dataset.id = card.id;
+
+            card.el = cardEl;
+
+            cardEl.dataset.id = card.matchId;
             cardEl.classList.remove('flipped', 'matched');
             const frontImg = cardEl.querySelector('.card-image img');
             frontImg.src = card.path;
             frontImg.alt = card.name;
-            cardEl.classList.remove('flipped');
 
+            cardEl.onclick = () => this.clickCard(card)
+
+            // cardEl.addEventListener('click', () => {
+            //     this.clickCard(card);
+            //     // card.el.classList.toggle('flipped');
+            //     // card.isFlipped = !card.isFlipped;
+            // })
         })
 
         const shuffledCards = this.cards;
     }
 
+    clickCard(card) {
+        // Block further clicking temporarily
+        if (this.isProcessing === true) {
+            return;
+        }
 
+        if (this.flippedCard.includes(card) || card.el.classList.contains('flipped')) {
+            return;
+        }
 
+        // Enable card flipping and keep track of flipped cards
+        card.el.classList.toggle('flipped');
+        this.flippedCard.push(card);
+
+        if (this.flippedCard.length === 2) {
+            this.isProcessing = true;
+
+            setTimeout(() => {
+                const card1 = this.flippedCard[0];
+                const card2 = this.flippedCard[1];
+
+                // Check for a match; if there's a match, cards remain; if no match, then flip them back over - source: https://stackoverflow.com/questions/56283681/js-memory-card-game-how-to-prevent-user-flipping-more-then-2-cards-at-the-same
+
+                if (card1.matchId === card2.matchId) {
+                    card1.el.classList.add('matched');
+                    card2.el.classList.add('matched');
+                    this.matches++;
+                    
+                } else {
+                    card1.el.classList.remove('flipped');
+                    card2.el.classList.remove('flipped');
+                    this.mismatches++;
+                }
+
+                this.statsCounter();
+
+                // Reset processing state after a match/mismatch is checked for
+                this.isProcessing = false;
+                this.flippedCard = [];
+            }, 2000)
+        }
+    }
+
+    statsCounter() {
+        this.matchCounter.innerHTML = this.matches;
+        this.mismatchCounter.innerHTML = this.mismatches;
+    }
 }
-
-
 
 document.addEventListener('DOMContentLoaded', () => {
     const game = new Game(cards);
 })
+
+// Card flip resource - Youtube video: https://youtu.be/c_SMmEea8pw?si=MIW4-YePl5E2SAgR
